479e1e3bab610791ef6f7931f85f87284da08cf2
liu <514301669@qq.com>
2015-12-03 12:22:09 +0800

    项目初始化

---
+++ MxWeibo/MxWeibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MxWeibo
+//
+//  Created by liu on 15/12/3.
+//  Copyright © 2015年 vizz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MxWeibo/MxWeibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MxWeibo
+//
+//  Created by liu on 15/12/3.
+//  Copyright © 2015年 vizz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
